2f5602dab733c32def0e480f4376d597197450a8
mac <mac@Elvon.local>
2025-04-18 03:32:56 +0100

    minor

--- models/merch.js
+++ models/merch.js
@@ -1,14 +1,4 @@
 const mongoose=require('mongoose');
-const workers=new mongoose.Schema({
-    Name: { type: String, required: true }, // Add this field
-    Size: { type: Number, required: true ,enum:['S','M','L','XL']},
-    category:{type:String,require:true,enum:['CropT','CollarNeck','RoundNeck']},
-    Description: { type: String, required: false }, // Add this field
-    color: { type: String, required: false }, //
-
-
-});
-// const mongoose = require('mongoose');
 
 const productSchema = new mongoose.Schema({
     name: { type: String, required: true }, // Product name
@@ -20,5 +10,4 @@ const productSchema = new mongoose.Schema({
 
 module.exports = mongoose.model('Product', productSchema);
 
-module.exports=mongoose.model("Merch",workers);
 
